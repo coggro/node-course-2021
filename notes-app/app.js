cc38ebef5b2592bb8db65b7b62003b3ad55f7b7b
CG <8009705+coggro@users.noreply.github.com>
2021-05-28 14:27:44 -0400

    Through Section 5

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,77 @@
-import validator from 'validator'
+import chalk from 'chalk'
+import fs from 'fs'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { addNote, removeNote, listNotes, readNote } from './notes.js'
 
-import {getNotes} from './notes.js'
+// add, remove, read, list
 
-console.log(getNotes())
+// Create Add Command
+const args = yargs(hideBin(process.argv))
+  .command({
+    command: `add`,
+    description: `Add a new note`,
+    builder: {
+      title: {
+        describe: `Note title`, // Describe option for help
+        demandOption: true, // Require option
+        type: `string`, // Force a string value
+      },
+      body: {
+        describe: `Note body`,
+        demandOption: true,
+        type: `string`,
+      },
+    },
+    // Add argv to arguments here and use in fx
+    handler: (argv) => {
+      let { title, body } = argv
+      addNote(title, body)
+    },
+  })
+  .command({
+    command: `remove`,
+    description: `Remove a note.`,
+    builder: {
+      title: {
+        describe: `Note title`,
+        demandOption: true,
+        type: `string`,
+      },
+    },
+    handler: (argv) => {
+      let { title } = argv
+      removeNote(title)
+    },
+  })
+  // Setup command to support list (print placeholder for now)
+  .command({
+    command: `list`,
+    description: `List out all notes.`,
+    handler: () => {
+      listNotes()
+    },
+  })
+  // Setup command to support read (print placeholder for now)
+  .command({
+    command: `read`,
+    description: `Reading a note.`,
+    builder: {
+      title: {
+        describe: `Note title`,
+        demandOption: true,
+        type: `string`,
+      },
+    },
+    handler: (argv) => {
+      let { title } = argv
+      readNote(title)
+    },
+  })
+// Test work by running both and ensure correct output
 
-console.log(validator.isURL(`coggro@gmail.com`))
+// Accessing argv on the actual args object forces it to actually parse. If we don't do it here, it won't parse.
+// console.log(args.argv)
+
+// Force a parse without logging
+args.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,73 @@
-const getNotes = () => {
-  return "Your notes..."
+import fs from 'fs'
+import chalk from 'chalk'
+
+const addNote = (title, body) => {
+  const notes = loadNotes()
+
+  const duplicateNote = notes.find((note) => note.title === title)
+  // const duplicateNotes = notes.filter((note) => {
+  //   return note.title === title
+  // })
+
+  if (!duplicateNote) {
+    notes.push({ title, body })
+    console.log(`Note added.`)
+  } else {
+    console.log(`Note title taken.`)
+  }
+  saveNotes(notes)
 }
 
-export { getNotes }
+const removeNote = (title) => {
+  const notes = loadNotes()
+
+  const remainingNotes = notes.filter((note) => {
+    return note.title !== title
+  })
+
+  saveNotes(remainingNotes)
+
+  if (remainingNotes.length < notes.length) {
+    console.log(chalk.green.inverse(`Note removed.`))
+  } else {
+    console.log(
+      chalk.red.inverse(
+        `No note exists with that title. No notes have been removed.`
+      )
+    )
+  }
+}
+
+const listNotes = () => {
+  const notes = loadNotes()
+
+  console.log(chalk.green.inverse(`Your Notes`))
+  notes.forEach((note) => {
+    console.log(note.title)
+  })
+}
+
+const readNote = (title) => {
+  const notes = loadNotes()
+  const targetNote = notes.find((note) => note.title === title)
+
+  if (targetNote) {
+    console.log(chalk.green.inverse(targetNote.title))
+    console.log(targetNote.body)
+  } else {
+    console.log(chalk.red.inverse(`Note not found.`))
+  }
+}
+
+const loadNotes = () => {
+  try {
+    return JSON.parse(fs.readFileSync(`notes.json`).toString())
+  } catch (e) {
+    return []
+  }
+}
+
+const saveNotes = (notes) => {
+  fs.writeFileSync(`notes.json`, JSON.stringify(notes))
+}
+export { addNote, removeNote, listNotes, readNote }
